2faa618ba7976bbd99d6a65daa76506357c2a2c5
smelge <traildogfilms@gmail.com>
2018-09-14 13:04:46 +0100

    Tests 1 - 5 Added

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,18 @@
+# Shortcut variables
+
+
 def pet_shop_name(shop_input)
   return shop_input[:name]
+end
+
+def total_cash(shop_input)
+  return shop_input[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop_input,money)
+  shop_input[:admin][:total_cash] += money
+end
+
+def pets_sold(shop_input)
+  return shop_input[:admin][:pets_sold]
 end
